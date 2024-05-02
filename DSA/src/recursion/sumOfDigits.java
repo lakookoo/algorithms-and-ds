6d60abf52b1a1bb0e11c8fc20edc0a3e8b8bd261
Larisa <kosareva.larisa@gmail.com>
2024-05-02 11:49:10 +0200

    sum of digits iteratively

--- DSA/src/recursion/sumOfDigits.java
+++ DSA/src/recursion/sumOfDigits.java
@@ -6,9 +6,18 @@ public class sumOfDigits {
     public static void main(String[] args) {
         System.out.println(sumOfDig(1345));
     }
-    
+    // O(log10(n)) - logarithm with the base 10
     public static int sumOfDig(int n){
         if(n == 0) return 0;
         return  n%10+sumOfDig(n/10);
+    }
+    // O(log10(n))
+    public static int sumOfDig2(int n){
+        int sum = 0;
+        while( n != 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 }
