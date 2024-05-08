58f4adcf959a1cbce4006b3b9f03d04c5b347276
Larisa <kosareva.larisa@gmail.com>
2024-05-08 19:31:04 +0200

    computing d value

--- DSA/src/cracking/cubicEquation.java
+++ DSA/src/cracking/cubicEquation.java
@@ -3,16 +3,17 @@ package cracking;
 
 public class cubicEquation {
     public static void main(String[] args) {
-
+        int d;
         for (int a = 1; a <= 1000; a++) {
             for (int b = 1; b <= 1000; b++) {
                 for (int c = 1; c <= 1000; c++) {
-                    for (int d = 1; d <= 1000; d++) {
-                        if (Math.pow(a, 3) + Math.pow(b, 3) == Math.pow(c, 3) + Math.pow(d, 3)) {
-                            System.out.println("Solution found: a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);
-                            break;
-                        }
+                    d = (int)Math.pow(Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3), 1 / 3);
+
+                    if (Math.pow(a, 3) + Math.pow(b, 3) == Math.pow(c, 3) + Math.pow(d, 3)&& 0 < d && d <= 1000) {
+                        System.out.println("Solution found: a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);
+                        
                     }
+
                 }
             }
         }
